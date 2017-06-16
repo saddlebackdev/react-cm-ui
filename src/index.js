ed00c79cf2397b9ebfc946e4f023da3b66fb5964
Cameron <morethanfire@icloud.com>
2017-06-16 13:39:16 -0700

    fixed path to components in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 // Collections
-export { Grid } from 'Collections/Grid.react.js';
-export { Table } from 'Collections/Table.react.js';
-export { Tabs } from 'Collections/Tabs.react.js';
+export { Grid } from './Collections/Grid.react.js';
+export { Table } from './Collections/Table.react.js';
+export { Tabs } from './Collections/Tabs.react.js';
 
 // Elements
-export { Button } from 'Elements/Button.react.js';
-export { Checkbox } from 'Elements/Checkbox.react.js';
-export { Container } from 'Elements/Container.react.js';
-export { Divider } from 'Elements/Divider.react.js';
-export { Header } from 'Elements/Header.react.js';
-export { Icon } from 'Elements/Icon.react.js';
-export { Input } from 'Elements/Input.react.js';
-export { Label } from 'Elements/Label.react.js';
-export { List } from 'Elements/List.react.js';
-export { Loader } from 'Elements/Loader.react.js';
-export { Radio } from 'Elements/Radio.react.js';
-export { TextArea } from 'Elements/TextArea.react.js';
+export { Button } from './Elements/Button.react.js';
+export { Checkbox } from './Elements/Checkbox.react.js';
+export { Container } from './Elements/Container.react.js';
+export { Divider } from './Elements/Divider.react.js';
+export { Header } from './Elements/Header.react.js';
+export { Icon } from './Elements/Icon.react.js';
+export { Input } from './Elements/Input.react.js';
+export { Label } from './Elements/Label.react.js';
+export { List } from './Elements/List.react.js';
+export { Loader } from './Elements/Loader.react.js';
+export { Radio } from './Elements/Radio.react.js';
+export { TextArea } from './Elements/TextArea.react.js';
 
 // Modules
-export { Accordion } from 'Modules/Accordion.react.js';
-export { DatePicker } from 'Modules/DatePicker.react.js';
-export { Drawer } from 'Modules/Drawer.react.js';
-export { Dropdown } from 'Modules/Dropdown.react.js';
-export { Modal } from 'Modules/Modal.react.js';
-export { SegmentedControls } from 'Modules/SegmentedControls.react.js';
-export { TimePicker } from 'Modules/TimePicker.react.js';
+export { Accordion } from './Modules/Accordion.react.js';
+export { DatePicker } from './Modules/DatePicker.react.js';
+export { Drawer } from './Modules/Drawer.react.js';
+export { Dropdown } from './Modules/Dropdown.react.js';
+export { Modal } from './Modules/Modal.react.js';
+export { SegmentedControls } from './Modules/SegmentedControls.react.js';
+export { TimePicker } from './Modules/TimePicker.react.js';
 
 // utils
-export { DatePickerUtils } from 'utils/DatePickerUtils.js';
-export { DateUtils } from 'utils/DateUtils.js';
-export { DOMUtils } from 'utils/DOMUtils.js';
-export { Utils } from 'utils/Utils.js';
+export { DatePickerUtils } from './utils/DatePickerUtils.js';
+export { DateUtils } from './utils/DateUtils.js';
+export { DOMUtils } from './utils/DOMUtils.js';
+export { Utils } from './utils/Utils.js';
 
 // Views
-export { Banner } from 'Views/Banner.react.js';
-export { Card } from 'Views/Card.react.js';
-export { SubNavigation } from 'Views/SubNavigation.react.js';
-export { TitleBar } from 'Views/TitleBar.react.js';
+export { Banner } from './Views/Banner.react.js';
+export { Card } from './Views/Card.react.js';
+export { SubNavigation } from './Views/SubNavigation.react.js';
+export { TitleBar } from './Views/TitleBar.react.js';
