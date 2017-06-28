72ab0aa3714bb9a88e29d77b155618244cae49cb
Geoffrey Roberts <groberts314@yahoo.com>
2017-06-28 16:42:40 -0700

    Atom is holding locks on my files and git flow is failing to merge.  Ugh!

--- docs/src/js/components/StyleGuideColors.react.js
+++ docs/src/js/components/StyleGuideColors.react.js
@@ -19,33 +19,33 @@ export default class StyleGuideColors extends React.Component {
                 </Header>
 
                 <Grid stackable={true}>
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch border={true} color="#fff" name="white" />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch border={true} color="#f6f7f8" name="grey 1" />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#edf1f5" name="grey 2" />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch border={true} color="#dbe0e3" name="grey 3" />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#97a4ab" name="grey 4" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#38424d" name="grey 5" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#1c2530" name="grey 6" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
                 </Grid>
 
                 {/* Secondary Colors */}
@@ -54,55 +54,55 @@ export default class StyleGuideColors extends React.Component {
                 </Header>
 
                 <Grid stackable={true}>
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#134174" name="blue" textInverse={true} />
                         <Swatch color="#00254d" name="dark blue" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#00aeef" name="cyan" textInverse={true} />
                         <Swatch color="#1c93c4" name="dark cyan" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#6dcff6" name="sky" textInverse={true} />
                         <Swatch color="#4095b6" name="dark sky" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#56c4c4" name="teal" textInverse={true} />
                         <Swatch color="#329594" name="dark teal" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#54cd86" name="green" textInverse={true} />
                         <Swatch color="#398f5d" name="green cyan" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#f99e49" name="orange" textInverse={true} />
                         <Swatch color="#c96d20" name="dark orange" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#f27a4d" name="red orange" textInverse={true} />
                         <Swatch color="#d94710" name="dark red orange" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#ee4334" name="red" textInverse={true} />
                         <Swatch color="#c2241a" name="dark red" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#eb4ea1" name="pink" textInverse={true} />
                         <Swatch color="#c23680" name="dark pink" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
 
-                    <GridColumn width={12} tablet={4} laptop={3}>
+                    <Grid.Column width={12} tablet={4} laptop={3}>
                         <Swatch color="#c68ef6" name="purple" textInverse={true} />
                         <Swatch color="#9958d2" name="dark purple" textInverse={true} />
-                    </GridColumn>
+                    </Grid.Column>
                 </Grid>
             </main>
         );
