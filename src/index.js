3f09a08e46fcc8326f347e6e7107875faf7f4b99
Cameron Brewer <morethanfire@icloud.com>
2018-06-14 17:42:27 -0700

    rail component and updates to checkbox.
    - New **Rail** component.
    - New `labelClassName` and `labelWeight` for **Checkbox**.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ export { default as Label } from './Elements/Label.react.js';
 export { default as List } from './Elements/List.react.js';
 export { default as Loader } from './Elements/Loader.react.js';
 export { default as Radio } from './Elements/Radio.react.js';
+export { default as Rail } from './Elements/Rail.react.js';
 export { default as TextArea } from './Elements/TextArea.react.js';
 
 // Modules
