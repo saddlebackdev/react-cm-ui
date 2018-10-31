0ac30a4eeca040bcdf47d8a2d834311f4907a416
Geoffrey Roberts <groberts314@yahoo.com>
2018-10-31 11:08:51 -0700

    Set icon title for comment menu dropdown and update comment docs page

--- docs/src/js/components/ElementsComment.react.js
+++ docs/src/js/components/ElementsComment.react.js
@@ -31,8 +31,8 @@ export default class CommentSample extends React.Component {
         super(props);
 
         this.state = {
-            editableCommentText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.',
-            editableComment2Text: 'Pellentesque sagittis quam enim, a euismod nisl tristique ac. Duis rutrum accumsan nisl sit amet congue. Vivamus ut convallis velit, nec adipiscing mi. Sed semper dui ut velit eleifend tincidunt.',
+            editableCommentText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            editableComment2Text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             isRemoveBannerOpen: false,
             isSaveBannerOpen: false
         };
@@ -58,14 +58,25 @@ export default class CommentSample extends React.Component {
 
                 <Comment
                     canDelete
-                    canEdit
+                    canEdit={false}
                     detailsPosition="right"
                     isEditable
                     name="Jessica Jones"
                     onDelete={this._onRemoveComment}
+                    time={1536941520}
+                >
+                    {'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+                </Comment>
+
+                <Comment
+                    canDelete
+                    canEdit
+                    isEditable
+                    name="Joe Smith"
+                    onDelete={this._onRemoveComment}
                     onSaveEdit={this._onSaveComment2}
                     text={editableComment2Text}
-                    time={1536941520}
+                    time={1536941640}
                 >
                     {editableComment2Text}
                 </Comment>
@@ -231,11 +242,31 @@ export default class ElmentsComment extends React.Component {
                     </Header.Subheader>
                 </Header>
 
-                <p>When the comment details are right-aligned, the edit and delete actions are left-aligned.</p>
+                <p>
+                    Use the <code>isEditable</code> prop to set whether or not a comment is editable or deletable
+                    <em>at all</em> (versus simply being read-only).  <code>isEditable</code> defaults to false, so set
+                    it to true for editable comments.
+                </p>
+
+                <p>
+                    Likely there will be some business rules around <em>who</em> may edit and delete comments.  For
+                    example, let's say the rules are that a comment may only be edited by its original author, but that
+                    certain "moderator" users are authorized to delete comments (e.g. comments deemed "inappropriate").
+                    You should use the <code>canDelete</code> and <code>canEdit</code> props to drive these kinds of
+                    permissions and selectively make deletion and editing available. The example below demonstrates
+                    this: Joe Smith's comments are editable and deleteable; Jessica Jone's comment is only deletable
+                    (i.e. let's say that Joe Smith is the logged in user and has moderation privileges.)
+                </p>
+
+                <p>
+                    When the comment details are right-aligned, the edit and delete action menu is left-aligned, and
+                    vice versa.
+                </p>
 
                 <Comment
                     canDelete
                     canEdit
+                    detailsPosition="right"
                     isEditable
                     name="Joe Smith"
                     onDelete={this._onRemoveComment}
@@ -248,14 +279,25 @@ export default class ElmentsComment extends React.Component {
 
                 <Comment
                     canDelete
-                    canEdit
-                    detailsPosition="right"
+                    canEdit={false}
                     isEditable
                     name="Jessica Jones"
                     onDelete={this._onRemoveComment}
+                    time={1536941520}
+                >
+                    {'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+                </Comment>
+
+                <Comment
+                    canDelete
+                    canEdit
+                    detailsPosition="right"
+                    isEditable
+                    name="Joe Smith"
+                    onDelete={this._onRemoveComment}
                     onSaveEdit={this._onSaveComment2}
                     text={editableComment2Text}
-                    time={1536941520}
+                    time={1536941640}
                 >
                     {editableComment2Text}
                 </Comment>
