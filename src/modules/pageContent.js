514bc60d2cca64102e315f4da8f4706ff1269f0a
IlyaRadinsky <ilya_radinsky@hotmail.com>
2019-09-12 14:00:12 +0300

    Added 'scrollable' prop for PageContent

--- src/modules/pageContent.js
+++ src/modules/pageContent.js
@@ -6,11 +6,12 @@ import React from 'react';
 import Utils from '../utils/utils.js';
 
 const PageContent = (props) => {
-    const { as, children, className, isFiltersRailOpen, style } = props;
+    const { as, children, className, isFiltersRailOpen, scrollable, style } = props;
     const winWidth = window.innerWidth;
     const isMobile = winWidth <= 767;
     const containerClasses = ClassNames('ui', 'page--content', className, {
         'page--content-filters_rail_open': !isMobile && isFiltersRailOpen,
+        'page--content-scrollable': scrollable,
     });
     const ElementType = Utils.getElementType(as, props);
 
@@ -24,6 +25,7 @@ const PageContent = (props) => {
 PageContent.defaultProps = {
     as: 'section',
     isFiltersRailOpen: false,
+    scrollable: false,
 };
 
 const asEnums = [ 'div', 'header', 'main', 'section' ];
@@ -32,6 +34,7 @@ PageContent.propTypes = {
     as: PropTypes.oneOf(asEnums),
     className: PropTypes.string,
     isFiltersRailOpen: PropTypes.bool,
+    scrollable: PropTypes.bool,
     style: PropTypes.object,
 };
 
