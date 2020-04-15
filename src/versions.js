6a71eee99876bf3dcd529a6c64cd270e664a1a9c
Cameron Brewer <morethanfire@icloud.com>
2020-04-14 17:49:39 -0700

    updated versions.js

--- src/versions.js
+++ src/versions.js
@@ -1,6 +1,16 @@
 const versions = {
     'react-cm-ui': {
-        package: '9.7.1',
+        package: '9.8.0',
+        components: {
+            dataDisplay: {
+                banner: 'NA',
+                divided: 'NA',
+                icon: 'NA',
+                image: 'NA',
+                list: 'NA',
+                table: 'NA',
+            },
+        },
         theme: {
             index: '1.0.0',
             breakpoints: '1.0.0',
