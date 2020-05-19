99c078abb33d36e933fd31323d5972db95456939
Geoffrey Roberts <groberts314@yahoo.com>
2020-05-19 16:10:21 -0700

    feat: add additional tests to `src/atoms/__test__/a.test.js`
    Also renamed folder from `__tests__` (plural) to `__test__` (singular)

---
+++ src/atoms/__test__/a.test.js
@@ -0,0 +1,25 @@
+/**
+ * To Run this test, from the React CM UI root folder, execute the following command:
+ * npx jest ./src/atoms/__test__/a.test.js
+ */
+import { shallow } from 'enzyme';
+import React from 'react';
+import A from '../a';
+
+describe('<A />', () => {
+    const props = {
+        onClick: jest.fn(),
+    };
+
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    const wrapper = shallow(<A {...props} />);
+
+    it('Should render without problems', () => {
+        expect(wrapper).toBeDefined();
+    });
+
+    it('Fires onClick()', () => {
+        wrapper.simulate('click');
+        expect(props.onClick).toHaveBeenCalledTimes(1);
+    });
+});
