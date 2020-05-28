e31e93f460bdc797fc40e32e2869ade9de565389
Cameron Brewer <morethanfire@icloud.com>
2020-05-28 15:01:37 -0700

    feat: banner and indexes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,6 +9,12 @@ const theme = {
         keys: breakpointsKeys,
         values: breakpointsValues,
     }),
+    zIndex: {
+        drawer: 10000,
+        modal: 11000,
+        prompt: 12000,
+        banner: 13000,
+    },
 };
 
 export default theme;
