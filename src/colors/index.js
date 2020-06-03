78dab928501e185437f2bc9a0549a21778467489
Cameron Brewer <morethanfire@icloud.com>
2020-06-03 14:53:08 -0700

    Merge pull request #151 from saddlebackdev/feature/button_save
    Added Palette, Shape, Transitions, and Typography to the Theme

---
+++ src/colors/index.js
@@ -0,0 +1,13 @@
+export { default as blue } from './blue';
+export { default as common } from './common';
+export { default as cyan } from './cyan';
+export { default as green } from './green';
+export { default as grey } from './grey';
+export { default as index } from './index';
+export { default as orange } from './orange';
+export { default as pink } from './pink';
+export { default as purple } from './purple';
+export { default as red } from './red';
+export { default as redOrange } from './redOrange';
+export { default as sky } from './sky';
+export { default as teal } from './teal';
---
+++ src/styles/createTypography.js
@@ -0,0 +1,40 @@
+const fontFamily = '"Source Sans Pro", sans-serif';
+const fontSize = 14; // px
+const fontWeightBold = 700;
+const fontWeightMedium = 600;
+const fontWeightRegular = 400;
+const htmlFontSize = 16; // px
+
+const pxToRem = (pxNum) => `${(pxNum / htmlFontSize) * (fontSize / 14)}rem`;
+
+export default function createTypography() {
+    const buildVariant = (fontWeight, size, lineHeight) => ({
+        fontFamily,
+        fontWeight,
+        fontSize: pxToRem(size),
+        lineHeight,
+    });
+
+    const variants = {
+        h1: buildVariant(fontWeightBold, 24, 1.5),
+        h2: buildVariant(fontWeightBold, 20, 1.5),
+        h3: buildVariant(fontWeightBold, 18, 1.5),
+        h4: buildVariant(fontWeightMedium, 16, 1.25),
+        h5: buildVariant(fontWeightRegular, 14, 1.5),
+        h6: buildVariant(fontWeightRegular, 12, 1.5),
+        body1: buildVariant(fontWeightRegular, 16, 1.5),
+        body2: buildVariant(fontWeightRegular, 14, 1.43),
+        caption: buildVariant(fontWeightRegular, 14, 1.66),
+    };
+
+    return {
+        pxToRem,
+        fontFamily,
+        fontSize,
+        fontWeightBold,
+        fontWeightMedium,
+        fontWeightRegular,
+        htmlFontSize,
+        ...variants,
+    };
+}
