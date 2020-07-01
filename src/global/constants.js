9429a2d3bc228029b64db66b243e577be5e288a3
Cameron Brewer <morethanfire@icloud.com>
2020-06-30 17:39:46 -0700

    feat: added classes to email and telephone link components

--- src/global/constants.js
+++ src/global/constants.js
@@ -1,2 +1,6 @@
-export const UI_CLASS_NAME = 'cmui';
 export const ENTER_KEY_CODE = 13;
+
+export const UI_CLASS_NAME = 'cmui';
+
+export const EMAIL_LINK_CLASSES = `${UI_CLASS_NAME} telphone_link`;
+export const TELEPHONE_LINK_CLASSES = `${UI_CLASS_NAME} telphone_link`;
