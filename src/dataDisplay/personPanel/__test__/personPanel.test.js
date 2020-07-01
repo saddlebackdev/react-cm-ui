b26d57439a2e6e30bb9c97ed223fe2ab2583a10d
Cameron Brewer <morethanfire@icloud.com>
2020-06-30 18:40:52 -0700

    feat: added the rest of the PersonPanel unit test and cleaned up code

--- src/dataDisplay/personPanel/__test__/personPanel.test.js
+++ src/dataDisplay/personPanel/__test__/personPanel.test.js
@@ -1,31 +1,143 @@
 /**
- * To Run this test, from the React CM UI root folder, execute the following command:
- * npx jest ./src/atoms/__test__/a.test.js
+ * To run this test from the church-management/client folder, execute the following command:
+ * npx jest ./src/dataDisplay/personPanel/__test__/personPanel.test.js
  */
 import { describe, expect, it } from '@jest/globals';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import PersonPanel from '../personPanel';
+import PersonPanelDetails from '../personPanelDetails';
+import MockedTheme from '../../../testUtils/mockedTheme';
+import PersonPanelSummary from '../personPanelSummary';
 
 describe('<PersonPanel />', () => {
+    const minimalChildren = [
+        <PersonPanelSummary
+            key="summary"
+        >
+            Summary
+        </PersonPanelSummary>,
+        <PersonPanelDetails
+            key="details"
+        >
+            Details
+        </PersonPanelDetails>,
+    ];
     const props = {
-        id: 'some_block--some_element_name-some_modifier',
-        onClick: jest.fn(),
+        id: 'block_name--element_name-modifier',
+        isExpanded: false,
+        onChange: jest.fn(),
     };
 
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    const wrapper = shallow(<A {...props} />);
-
     it('Should render without problems', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
         expect(wrapper).toBeDefined();
     });
 
-    it('Should have expected \'id\' prop', () => {
-        expect(wrapper.prop('id')).toEqual(props.id);
+    it('should render with the root classes', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
+        const root = wrapper.find('div').first();
+
+        expect(root.hasClass('cmui')).toEqual(true);
+        expect(root.hasClass('person_panel')).toEqual(true);
+        expect(root.hasClass(/(makeStyles)-(root)-(\d+)/)).toEqual(true);
     });
 
-    it('Fires onClick()', () => {
-        wrapper.simulate('click');
-        expect(props.onClick).toHaveBeenCalledTimes(1);
+    it('Should have expected `id` prop', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
+        const root = wrapper.find('div').first();
+
+        expect(root.prop('id')).toEqual(props.id);
+    });
+
+    it('Should expand when clicking PersonPanelSummary', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                    onChange={undefined}
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
+        wrapper.find('PersonPanelSummary').simulate('click');
+
+        const summary = wrapper.find(PersonPanelSummary);
+
+        expect(summary.prop('isExpanded')).toEqual(true);
+        expect(summary.find('div').first().hasClass(/(makeStyles)-(isExpanded)-(\d+)/)).toEqual(true);
+
+        const details = wrapper.find(PersonPanelDetails);
+
+        expect(details.prop('isExpanded')).toEqual(true);
+        expect(details.find('div').first().hasClass(/(makeStyles)-(isExpanded)-(\d+)/)).toEqual(true);
+    });
+
+    it('Should call onChange when clicking PersonPanelSummary', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
+        wrapper.find('PersonPanelSummary').simulate('click');
+
+        expect(props.onChange).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should be controlled', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanel
+                    {...props}
+                    isExpanded
+                >
+                    {minimalChildren}
+                </PersonPanel>
+            </MockedTheme>,
+        );
+
+        const summary = wrapper.find(PersonPanelSummary);
+
+        expect(summary.prop('isExpanded')).toEqual(true);
+        expect(summary.find('div').first().hasClass(/(makeStyles)-(isExpanded)-(\d+)/)).toEqual(true);
+
+        const details = wrapper.find(PersonPanelDetails);
+
+        expect(details.prop('isExpanded')).toEqual(true);
+        expect(details.find('div').first().hasClass(/(makeStyles)-(isExpanded)-(\d+)/)).toEqual(true);
     });
 });
