670748cb0bb980ec2e31208e61cf806b142e27c7
Cameron Brewer <morethanfire@icloud.com>
2020-07-02 09:35:01 -0700

    feat: PersonPanelDetailsActionButton unit test (not finished)

---
+++ src/dataDisplay/personPanel/__test__/personPanelDetailsActionButton.test.js
@@ -0,0 +1,40 @@
+/**
+ * To run this test from the church-management/client folder, execute the following command:
+ * npx jest ./src/dataDisplay/personPanel/__test__/personPanel.test.js
+ */
+import { mount, shallow } from 'enzyme';
+import React from 'react';
+import PersonPanelDetailsActionButton from '../personPanelDetailsActionButton';
+import MockedTheme from '../../../testUtils/mockedTheme';
+
+describe('<PersonPanel />', () => {
+    const props = {
+        id: 'block_name--element_name-modifier',
+    };
+
+    it('Should render without problems', () => {
+        const wrapper = shallow(
+            <MockedTheme>
+                <PersonPanelDetailsActionButton
+                    {...props}
+                />
+            </MockedTheme>,
+        );
+
+        expect(wrapper).toBeDefined();
+    });
+
+    it('Should have expected `id` prop', () => {
+        const wrapper = mount(
+            <MockedTheme>
+                <PersonPanelDetailsActionButton
+                    {...props}
+                />
+            </MockedTheme>,
+        );
+
+        const root = wrapper.find(Button).first();
+
+        expect(root.prop('id')).toEqual(props.id);
+    });
+});
