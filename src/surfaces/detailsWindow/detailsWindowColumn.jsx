05039141fe76edeaa23b8a3e14524cc87ccfa14e
Cameron Brewer <morethanfire@icloud.com>
2020-08-05 09:47:48 -0700

    feat: moved some template components

---
+++ src/surfaces/detailsWindow/detailsWindowColumn.jsx
@@ -0,0 +1,158 @@
+import _ from 'lodash';
+import ClassNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { columnPropTypesShape, columnPropsPropTypesShape } from './detailsWindowPropTypes';
+import Button from '../../inputs/button';
+import Header from '../../dataDisplay/header';
+import Icon from '../../dataDisplay/icon';
+
+const propTypes = {
+    column: columnPropTypesShape.isRequired,
+    columnNumber: PropTypes.number,
+    columnProps: columnPropsPropTypesShape,
+    data: PropTypes.shape({}).isRequired,
+    isExpanded: PropTypes.bool,
+    onExpandButtonToggle: PropTypes.func,
+    moduleType: PropTypes.string,
+    setColumnContainerHeight: PropTypes.func,
+};
+
+const defaultProps = {
+    columnNumber: undefined,
+    columnProps: undefined,
+    isExpanded: false,
+    moduleType: undefined,
+    onExpandButtonToggle: undefined,
+    setColumnContainerHeight: undefined,
+};
+
+const nop = () => {};
+
+class DetailsWindowColumn extends React.PureComponent {
+    componentDidMount() {
+        const { setColumnContainerHeight } = this.props;
+
+        if (this.columnRef && _.isFunction(setColumnContainerHeight)) {
+            setColumnContainerHeight(this.columnRef.offsetHeight);
+        }
+    }
+
+    render() {
+        const {
+            column: {
+                accessor,
+                divide,
+                expandedButton,
+                expandedButtonId,
+                flexBasis,
+                flexGrow,
+                flexShrink,
+                fontSize,
+                fontWeight,
+                header,
+                style,
+                width,
+            },
+            columnNumber,
+            columnProps,
+            data,
+            isExpanded,
+            onExpandButtonToggle,
+            moduleType,
+        } = this.props;
+        const bemClassName = `${moduleType}--details_window_column`;
+        const containerClassName = `${bemClassName}${columnNumber ? '_inner' : ''}`;
+        const containerClasses = ClassNames(
+            containerClassName,
+            {
+                [`${containerClassName}-divide_left`]: divide === true || divide === 'left',
+                [`${containerClassName}-divide_right`]: divide === 'right',
+            },
+        );
+        const accessorClasses = ClassNames(`${bemClassName}_accessor`, {
+            'font-size-large': fontSize === 'large',
+            'font-size-medium': fontSize === 'medium',
+            'font-size-small': !fontSize || fontSize === 'small',
+            'font-weight-bold': !fontWeight || fontWeight === 'bold',
+            'font-weight-normal': fontWeight === 'normal',
+            'font-weight-semibold': fontWeight === 'semibold',
+        });
+
+        let horizontalSpacing;
+
+        if (columnProps) {
+            horizontalSpacing = columnProps.horizontalSpacing;
+        }
+
+        let accessedData;
+        let flexBasisInlineStyleValue;
+        let flexGrowInlineStyleValue;
+        let flexShrinkInlineStyleValue;
+        let horizontalSpacingInlineStyleValue;
+
+        if (_.isString(accessor)) {
+            accessedData = _.get(data, accessor);
+        } else if (_.isFunction(accessor)) {
+            accessedData = accessor(data);
+        }
+
+        if (columnNumber) {
+            flexBasisInlineStyleValue = flexBasis || 'auto';
+            flexGrowInlineStyleValue = flexGrow || 0;
+            flexShrinkInlineStyleValue = flexShrink || 0;
+            horizontalSpacingInlineStyleValue = horizontalSpacing ?
+                `${horizontalSpacing}px` :
+                null;
+        }
+
+        return (
+            <div
+                className={containerClasses}
+                style={({
+                    flexBasis: flexBasisInlineStyleValue,
+                    flexGrow: flexGrowInlineStyleValue,
+                    flexShrink: flexShrinkInlineStyleValue,
+                    paddingLeft: horizontalSpacingInlineStyleValue,
+                    paddingRight: horizontalSpacingInlineStyleValue,
+                    width: `${width}`,
+                    ...style,
+                })}
+                ref={(ref) => { this.columnRef = ref; }}
+            >
+                {expandedButton && (
+                    <Button
+                        className={`${bemClassName}_expandable_toggle_button`}
+                        color="light"
+                        icon
+                        id={expandedButtonId}
+                        onClick={onExpandButtonToggle || nop}
+                        outlined
+                    >
+                        <Icon
+                            compact
+                            rotate={isExpanded ? 180 : null}
+                            title={isExpanded ? 'Collapse' : 'Expand'}
+                            type="chevron-down"
+                        />
+                    </Button>
+                )}
+
+                {header && (
+                    <Header size="xsmall" style={{ margin: 0 }}>
+                        {header}
+                    </Header>
+                )}
+
+                <div className={accessorClasses}>
+                    {accessedData}
+                </div>
+            </div>
+        );
+    }
+}
+
+DetailsWindowColumn.propTypes = propTypes;
+DetailsWindowColumn.defaultProps = defaultProps;
+
+export default DetailsWindowColumn;
---
+++ src/templates/page/pageContent.jsx
@@ -0,0 +1,97 @@
+import ClassNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import makeStyles from '../../styles/makeStyles';
+import Utils from '../../utils/utils';
+
+const propTypes = {
+    as: PropTypes.oneOf(['div', 'header', 'main', 'section']),
+    children: PropTypes.node,
+    className: PropTypes.string,
+    id: PropTypes.string,
+    isFiltersRailOpen: PropTypes.bool,
+    scrollable: PropTypes.bool,
+    style: PropTypes.shape({}),
+};
+
+const defaultProps = {
+    as: 'section',
+    children: null,
+    className: null,
+    id: null,
+    isFiltersRailOpen: false,
+    scrollable: false,
+    style: null,
+};
+
+const useStyles = makeStyles((theme) => {
+    const {
+        breakpoints,
+        gutters,
+    } = theme;
+
+    return {
+        root: {
+            margin: `0 -${gutters.page.sm}`,
+            padding: `0 ${gutters.page.sm}`,
+            position: 'relative',
+            transition: 'margin 200ms ease-out',
+            zIndex: 0,
+            '&.page--content-filters_rail_open': {
+                marginLeft: 250,
+            },
+            '&.page--content-scrollable': {
+                overflowX: 'scroll',
+            },
+            [breakpoints.up(496)]: {
+                margin: `0 -${gutters.page[496]}`,
+                padding: `0 ${gutters.page[496]}`,
+            },
+        },
+    };
+});
+
+function PageContent(props) {
+    const {
+        as,
+        children,
+        className,
+        id,
+        isFiltersRailOpen,
+        scrollable,
+        style,
+    } = props;
+
+    const classes = useStyles(props);
+    const winWidth = window.innerWidth;
+    const isMobile = winWidth <= 767;
+    const bemName = 'page--content';
+
+    const rootClasses = ClassNames(
+        'ui',
+        classes.root,
+        bemName,
+        className,
+        {
+            [`${bemName}-filters_rail_open`]: !isMobile && isFiltersRailOpen,
+            [`${bemName}-scrollable`]: scrollable,
+        },
+    );
+
+    const ElementType = Utils.getElementType(as, props);
+
+    return (
+        <ElementType
+            className={rootClasses}
+            id={id}
+            style={style}
+        >
+            {children}
+        </ElementType>
+    );
+}
+
+PageContent.propTypes = propTypes;
+PageContent.defaultProps = defaultProps;
+
+export default PageContent;
