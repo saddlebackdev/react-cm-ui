9d764221d1c2b3832b05f14cab2fc4da68c070a0
Cameron Brewer <morethanfire@icloud.com>
2020-08-18 10:44:56 -0700

    docs: added DataGrid documentation

--- docs/src/dataDisplay/dataGrid/dataGrid.jsx
+++ docs/src/dataDisplay/dataGrid/dataGrid.jsx
@@ -4,9 +4,13 @@ import {
 } from 'react-cm-ui';
 import React from 'react';
 import ComponentApi from '../../global/componentApi';
+import DataGridSample from './dataGridSample';
+import DataGridSortableRowsSample from './dataGridSortableRowsSample';
+import DataGridStickyColumnsSample from './dataGridStickyColumnsSample';
+import Example from '../../global/example';
 import Heading from '../../global/heading';
-import MarkdownContainer from '../../global/markdownContainer';
 import Main from '../../global/main';
+import MarkdownContainer from '../../global/markdownContainer';
 /* eslint-disable import/no-named-default, import/extensions */
 import { default as dataGridDoc } from '!!@advclb/react-docgen-loader!react-cm-ui/dataDisplay/dataGrid/dataGrid';
 /* eslint-enable import/no-named-default, import/extensions */
@@ -31,9 +35,63 @@ function DocsDataGrid() {
                         anchorLink="example"
                         variant="h2"
                     >
-                        Coming soon.
+                        Basic Data Grid
                     </Heading>
+
+                    <Typography
+                        variant="body1"
+                    >
+                        The most basic thing you can do with a DataGrid.
+                    </Typography>
                 </MarkdownContainer>
+
+                <Example
+                    rawCode={require('!!raw-loader!./dataGridSample').default}
+                >
+                    <DataGridSample />
+                </Example>
+
+                <MarkdownContainer>
+                    <Heading
+                        anchorLink="children"
+                        variant="h2"
+                    >
+                        Sticky Columns
+                    </Heading>
+
+                    <Typography
+                        variant="body1"
+                    >
+                        An example to show the use of the DataGrid&apos;s `stickyColumns` prop.
+                    </Typography>
+                </MarkdownContainer>
+
+                <Example
+                    rawCode={require('!!raw-loader!./dataGridStickyColumnsSample').default}
+                >
+                    <DataGridStickyColumnsSample />
+                </Example>
+
+                <MarkdownContainer>
+                    <Heading
+                        anchorLink="children"
+                        variant="h2"
+                    >
+                        Sortable Rows
+                    </Heading>
+
+                    <Typography
+                        variant="body1"
+                    >
+                        An example to show the use of the DataGrid&apos;s `sortable` prop.
+                    </Typography>
+                </MarkdownContainer>
+
+                <Example
+                    rawCode={require('!!raw-loader!./dataGridSortableRowsSample').default}
+                >
+                    <DataGridSortableRowsSample />
+                </Example>
 
                 <ComponentApi
                     docs={[
