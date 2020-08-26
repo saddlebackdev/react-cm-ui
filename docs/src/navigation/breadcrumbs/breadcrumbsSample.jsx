327367fad9959486c78c1d07003eb7fcf90ff45e
oscar <oscarresendizuaq@gmail.com>
2020-08-26 10:41:05 -0500

    Navigation Breadcrumbs/Tabs docs updates to show the object parameters structure in the simple samples

--- docs/src/navigation/breadcrumbs/breadcrumbsSample.jsx
+++ docs/src/navigation/breadcrumbs/breadcrumbsSample.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
 import { Breadcrumbs } from 'react-cm-ui';
-import ROUTER from '../../../../src/navigation/breadcrumbs/__test__/breadcrumbsMockups';
+
+const router = { // Router mockup
+    location: {
+        pathname: '/my-section/sub-section/some-status/cool-stuff',
+    },
+    params: {},
+    push: (to) => { alert(`it would push to: ${to}`); },
+    routes: [
+        {
+            path: '/',
+            indexRoute: {
+                title: 'Home',
+            },
+            childRoutes: [
+                {
+                    path: 'my-section',
+                    indexRoute: {
+                        title: 'My Section',
+                    },
+                    childRoutes: [
+                        {
+                            path: 'sub-section',
+                            indexRoute: {
+                                title: 'Sub Section',
+                            },
+                            childRoutes: [
+                                {
+                                    path: 'some-status',
+                                    indexRoute: {
+                                        title: 'Some Status',
+                                    },
+                                    childRoutes: [
+                                        {
+                                            path: 'cool-stuff',
+                                            title: 'Cool Stuff',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
 
 function BreadcrumbsSample() {
     return (
         <Breadcrumbs
-            router={ROUTER}
+            router={router}
         />
     );
 }
