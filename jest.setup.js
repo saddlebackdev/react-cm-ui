324955ab443361d182710ee5d40009825adf246b
Cameron Brewer <morethanfire@icloud.com>
2020-08-27 14:28:33 -0700

    feat: removing a parentStyleSheet warning

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,16 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-console
+const originalWarn = console.warn.bind(console.warn);
+
+beforeAll(() => {
+    // eslint-disable-next-line no-console
+    console.warn = (msg) => !msg.toString().includes('parentStyleSheet') && originalWarn(msg);
+});
+
+afterAll(() => {
+    // eslint-disable-next-line no-console
+    console.warn = originalWarn;
+});
