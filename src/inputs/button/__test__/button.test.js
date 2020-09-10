cd409fa64997921976a146c8da21153bfe382404
grizzlyAlex <alejosod@gmail.com>
2020-09-10 14:57:11 -0500

    added button tests

--- src/inputs/button/__test__/button.test.js
+++ src/inputs/button/__test__/button.test.js
@@ -13,4 +13,54 @@ describe('<Button />', () => {
 
         expect(wrapper).toBeDefined();
     });
+
+    it('Should be disable', () => {
+        const wrapper = shallow(
+            <MockedTheme>
+                <Button disable id={'disableButton'} >children</Button>
+            </MockedTheme>
+        );
+
+        const isDisabled = wrapper.html().includes('colorDisable')
+
+        expect(isDisabled).toBe(true);
+    })
+
+    it('Should be a fluid button', () => {
+        const wrapper = shallow(
+            <MockedTheme>
+                <Button fluid id={'fluidButton'} >children</Button>
+            </MockedTheme>
+        );
+
+        const isDisabled = wrapper.html().includes('fluid')
+
+        expect(isDisabled).toBe(true);
+    })
+
+    it('Should be pill button', () => {
+        const wrapper = shallow(
+            <MockedTheme>
+                <Button pill id={'fluidButton'} >children</Button>
+            </MockedTheme>
+        );
+
+        console.log(wrapper.html())
+        const isPill = wrapper.html().includes('Button-pill')
+
+        expect(isPill).toBe(true);
+    })
+
+    it('Should be outlined button', () => {
+        const wrapper = shallow(
+            <MockedTheme>
+                <Button outline id={'fluidButton'} >children</Button>
+            </MockedTheme>
+        );
+
+        const isOutlined = wrapper.html().includes('Button-outlined')
+
+        expect(isOutlined).toBe(true);
+    })
+
 });
