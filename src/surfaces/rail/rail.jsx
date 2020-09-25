c27fac6b2d8eac6ea013c81106168662362ba975
Cameron Brewer <morethanfire@icloud.com>
2020-09-25 08:21:59 -0700

    feat: converted rail's scss to jss

--- src/surfaces/rail/rail.jsx
+++ src/surfaces/rail/rail.jsx
@@ -1,32 +1,96 @@
-
 import ClassNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import withStyles from '../../styles/withStyles';
 
+const propTypes = {
+    children: PropTypes.node,
+    classes: PropTypes.shape({
+        root: PropTypes.string,
+        positionLeft: PropTypes.string,
+        positionRight: PropTypes.string,
+    }),
+    className: PropTypes.string,
+    position: PropTypes.oneOf(['left', 'right']),
+};
+
+const defaultProps = {
+    children: null,
+    classes: null,
+    className: null,
+    position: 'left',
+};
+
+const styles = (theme) => ({
+    positionLeft: {},
+    positionRight: {},
+    root: {
+        backgroundColor: theme.palette.background.primary,
+        height: '100%',
+        padding: 22,
+        width: 250,
+        '&$positionLeft, &$positionRight': {
+            '&::after': {
+                content: '""',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                width: 15,
+            },
+        },
+        '&$positionLeft': {
+            boxShadow: `inset -1px 0 0 0 ${theme.palette.border.primary}`,
+            left: 0,
+            top: 0,
+            '&::after': {
+                background: 'linear-gradient(to right, rgba(0, 0, 0, .1) -100%, transparent 100%)',
+                right: -15,
+            },
+        },
+        '&$positionRight': {
+            boxShadow: `inset 1px 0 0 0 ${theme.palette.border.primary}`,
+            right: 0,
+            top: 0,
+            '&::after': {
+                background: 'linear-gradient(to left, rgba(0, 0, 0, .1) -100%, transparent 100%)',
+                left: -15,
+            },
+        },
+    },
+});
+
+// eslint-disable-next-line react/prefer-stateless-function
 class Rail extends React.Component {
     render() {
         const {
-            children, className, position, style,
+            classes,
+            children,
+            className,
+            position,
         } = this.props;
-        const containerClasses = ClassNames('ui', 'rail', className, {
-            'rail-position-left': position === 'left',
-            'rail-position-right': position === 'right',
-        });
+
+        const rootClasses = ClassNames(
+            'ui',
+            'rail',
+            classes.root,
+            className,
+            {
+                [classes.positionLeft]: position === 'left',
+                [classes.positionRight]: position === 'right',
+            },
+        );
 
         return (
-            <div className={containerClasses} style={style}>
+            <div
+                className={rootClasses}
+            >
                 {children}
             </div>
         );
     }
 }
 
-const positionEnums = ['left', 'right'];
+Rail.propTypes = propTypes;
+Rail.defaultProps = defaultProps;
 
-Rail.propTypes = {
-    className: PropTypes.string,
-    position: PropTypes.oneOf(positionEnums).isRequired,
-    style: PropTypes.shape({}),
-};
-
-export default Rail;
+export default withStyles(styles)(Rail);
