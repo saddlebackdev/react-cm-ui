88f1bbb42ea251897e9b6a83af59d5b6ffde2bfc
Cameron Brewer <morethanfire@icloud.com>
2020-12-17 14:59:44 -0800

    feat: pr and bad merge fixes

--- docs/src/inputs/prompt/devSandbox/examples/exampleDefaultPrompt.jsx
+++ docs/src/inputs/prompt/devSandbox/examples/exampleDefaultPrompt.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
 import {
     A,
     Button,
     Prompt,
 } from 'react-cm-ui';
+import {
+    noop,
+} from 'lodash';
+import React from 'react';
 
 function ExampleDefaultPrompt() {
     return (
         <div>
             <Prompt inline>
-                <Button color="success">Save Me!</Button>
+                <Button
+                    color="success"
+                    tabIndex={0}
+                >
+                    Save Me!
+                </Button>
             </Prompt>
 
             <br />
             <br />
 
-            <Prompt inline>
-                <A>Link</A>
+            <Prompt
+                inline
+            >
+                <A
+                    onClick={noop}
+                    tabIndex={0}
+                >
+                    Link
+                </A>
             </Prompt>
         </div>
     );
