eb9b4515a877e904e63dc1bca41e023c130f7c5b
Cameron Brewer <morethanfire@icloud.com>
2021-04-19 15:52:33 -0700

    feat: fixed gridRowDeprecated styles

--- src/layout/grid/gridRowDeprecated.jsx
+++ src/layout/grid/gridRowDeprecated.jsx
@@ -2,11 +2,11 @@ import ClassNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
+    BEM_GRID_COLUMN,
     BEM_GRID_ROW,
     UI_CLASS_NAME,
 } from '../../global/constants';
 import makeStyles from '../../styles/makeStyles';
-import Utils from '../../utils/utils';
 
 const columnNumberEnums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const xAlignEnums = ['center', 'left', 'right'];
@@ -36,6 +36,18 @@ const defaultProps = {
 };
 
 const useStyles = makeStyles(() => ({
+    'deprecatedGridRowColumns-1': {},
+    'deprecatedGridRowColumns-2': {},
+    'deprecatedGridRowColumns-3': {},
+    'deprecatedGridRowColumns-4': {},
+    'deprecatedGridRowColumns-5': {},
+    'deprecatedGridRowColumns-6': {},
+    'deprecatedGridRowColumns-7': {},
+    'deprecatedGridRowColumns-8': {},
+    'deprecatedGridRowColumns-9': {},
+    'deprecatedGridRowColumns-10': {},
+    'deprecatedGridRowColumns-11': {},
+    'deprecatedGridRowColumns-12': {},
     deprecatedHorizontalCenter: {},
     deprecatedHorizontalLeft: {},
     deprecatedHorizontalTop: {},
@@ -53,6 +65,42 @@ const useStyles = makeStyles(() => ({
         flexWrap: 'wrap',
         justifyContent: 'inherit',
         width: '100%',
+        [`&$deprecatedGridRowColumns-1 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 1) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-2 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 2) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-3 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 3) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-4 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 4) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-5 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 5) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-6 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 6) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-7 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 7) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-8 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 8) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-9 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 9) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-10 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 10) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-11 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 11) * 10e7) / 10e5}%`,
+        },
+        [`&$deprecatedGridRowColumns-12 .${BEM_GRID_COLUMN}`]: {
+            width: `${Math.round((1 / 12) * 10e7) / 10e5}%`,
+        },
         '&$deprecatedHorizontalCenter': {
             justifyContent: 'center',
         },
@@ -106,17 +154,17 @@ const GridRowDeprecated = React.forwardRef(
             classes.root,
             className,
             {
-                [`grid-row-columns-${Utils.numberToWord(columns)}`]: columns,
-                [classes.horizontalAlignCenter]: horizontalAlign === 'center',
-                [classes.horizontalAlignLeft]: horizontalAlign === 'left',
-                [classes.horizontalAlignRight]: horizontalAlign === 'right',
-                [classes.stressed]: stressed,
-                [classes.textAlignCenter]: textAlign === 'center',
-                [classes.textAlignLeft]: textAlign === 'left',
-                [classes.textAlignRight]: textAlign === 'right',
-                [classes.verticalAlignBottom]: verticalAlign === 'bottom',
-                [classes.verticalAlignMiddle]: verticalAlign === 'middle',
-                [classes.verticalAlignTop]: verticalAlign === 'top',
+                [classes[`deprecatedGridRowColumns-${String(columns)}`]]: columns,
+                [classes.deprecatedHorizontalAlignCenter]: horizontalAlign === 'center',
+                [classes.deprecatedHorizontalAlignLeft]: horizontalAlign === 'left',
+                [classes.deprecatedHorizontalAlignRight]: horizontalAlign === 'right',
+                [classes.deprecatedStressed]: stressed,
+                [classes.deprecatedTextAlignCenter]: textAlign === 'center',
+                [classes.deprecatedTextAlignLeft]: textAlign === 'left',
+                [classes.deprecatedTextAlignRight]: textAlign === 'right',
+                [classes.deprecatedVerticalAlignBottom]: verticalAlign === 'bottom',
+                [classes.deprecatedVerticalAlignMiddle]: verticalAlign === 'middle',
+                [classes.deprecatedVerticalAlignTop]: verticalAlign === 'top',
             },
         );
 
