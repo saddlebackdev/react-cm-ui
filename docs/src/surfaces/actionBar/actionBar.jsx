3bdb8092f79de5c9ca361382318591ec4baa396b
Cameron Brewer <morethanfire@icloud.com>
2021-05-19 13:23:31 -0700

    docs(feat): actionbar fixes

--- docs/src/surfaces/actionBar/actionBar.jsx
+++ docs/src/surfaces/actionBar/actionBar.jsx
@@ -3,11 +3,10 @@ import {
 } from 'react-cm-ui';
 import {
     camelCase,
+    startCase,
 } from 'lodash';
 import React from 'react';
-import ActionBarSample from './ActionBarSample';
 import ComponentApi from '../../global/componentApi';
-import Example from '../../global/example';
 import Heading from '../../global/heading';
 import MarkdownContainer from '../../global/markdownContainer';
 import Main from '../../global/main';
@@ -36,21 +35,9 @@ function DocsActionBar() {
                         anchorLink="example"
                         variant="h2"
                     >
-                        Standard Action Bar
+                        Coming soon.
                     </Heading>
-
-                    <Typography
-                        variant="body1"
-                    >
-                        Contained button.
-                    </Typography>
                 </MarkdownContainer>
-
-                <Example
-                    rawCode={require('!!raw-loader!./actionBarSample').default}
-                >
-                    <ActionBarSample />
-                </Example>
 
                 <ComponentApi
                     docs={[
