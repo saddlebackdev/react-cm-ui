2b1d2b336c36e653224c56d2df2889afbe7931c9
Cameron Brewer <morethanfire@icloud.com>
2021-06-23 10:38:12 -0700

    feat: updated component description

--- src/feedback/snackbar/snackbar.jsx
+++ src/feedback/snackbar/snackbar.jsx
@@ -57,7 +57,9 @@ const useStyles = makeStyles(({
 }));
 
 /**
- * "Snackbars provide brief messages about app processes."
+ * Snackbars inform users of a process that an app has performed or will perform.
+ * They appear temporarily, towards the bottom of the screen. They shouldn’t interrupt the
+ * user experience, and they don’t require user input to disappear.
  */
 function Snackbar(props) {
     const {
