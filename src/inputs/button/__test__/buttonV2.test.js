e583e8361bfabd86437159d2720386b9991b0e4f
Cameron Brewer <morethanfire@icloud.com>
2021-08-31 17:25:05 -0700

    feat: clean up and test fix??

--- src/inputs/button/__test__/buttonV2.test.js
+++ src/inputs/button/__test__/buttonV2.test.js
@@ -4,10 +4,10 @@
  */
 
 import React from 'react';
-import Button from '../button';
+import ButtonV2 from '../buttonV2';
 import mountWithTheme from '../../../testUtils/enzymeHelpers';
 
-describe('<Button />', () => {
+describe('<ButtonV2 />', () => {
     const bemName = 'some_block--some_element_name';
     const text = 'hello world';
 
@@ -18,11 +18,11 @@ describe('<Button />', () => {
 
     it('should render without problems', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
         expect(wrapper.exists()).toBe(true);
@@ -30,11 +30,11 @@ describe('<Button />', () => {
 
     it('Should have expected \'children\'', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
         expect(wrapper.find('span').text()).toEqual(text);
@@ -44,14 +44,14 @@ describe('<Button />', () => {
         const rootOverride = 'makeStyles-root-123';
 
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
                 classes={{
                     root: rootOverride,
                 }}
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
         const rootNode = wrapper.find('button').first();
@@ -61,82 +61,82 @@ describe('<Button />', () => {
 
     it('Should render with proper classNames', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
         const rootNode = wrapper.find('button').first();
 
         expect(rootNode.hasClass('cmui')).toEqual(true);
         expect(rootNode.hasClass('button')).toEqual(true);
-        expect(rootNode.hasClass(/(Button)-(root)-(\d+)/)).toEqual(true);
+        expect(rootNode.hasClass(/(ButtonV2)-(root)-(\d+)/)).toEqual(true);
         expect(rootNode.hasClass(props.className)).toEqual(true);
     });
 
     it('Should have expected \'id\' prop', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
         expect(wrapper.prop('id')).toEqual(props.id);
     });
 
-    it('Should be \'disable\'', () => {
+    it('Should add expected class when \'disable\' prop is true', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
                 disable
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
-        expect(wrapper.find('button').hasClass(/(Button)-(disable)-(\d+)/)).toEqual(true);
+        expect(wrapper.find('button').hasClass(/(ButtonV2)-(disable)-(\d+)/)).toEqual(true);
     });
 
     it('Should be a \'fluid\' button', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
                 fluid
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
-        expect(wrapper.find('button').hasClass(/(Button)-(fluid)-(\d+)/)).toEqual(true);
+        expect(wrapper.find('button').hasClass(/(ButtonV2)-(fluid)-(\d+)/)).toEqual(true);
     });
 
-    it('Should be \'pill\' button', () => {
+    it('Should be a \'pill\' button', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
                 pill
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
-        expect(wrapper.find('button').hasClass(/(Button)-(pill)-(\d+)/)).toEqual(true);
+        expect(wrapper.find('button').hasClass(/(ButtonV2)-(pill)-(\d+)/)).toEqual(true);
     });
 
-    it('Should be \'outline\' button', () => {
+    it('Should be a \'outline\' button', () => {
         const wrapper = mountWithTheme(
-            <Button
+            <ButtonV2
                 {...props}
                 outline
             >
                 {text}
-            </Button>,
+            </ButtonV2>,
         );
 
-        expect(wrapper.find('button').hasClass(/(Button)-(outline)-(\d+)/)).toEqual(true);
+        expect(wrapper.find('button').hasClass(/(ButtonV2)-(outline)-(\d+)/)).toEqual(true);
     });
 });
