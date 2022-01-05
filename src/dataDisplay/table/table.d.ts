05a9e65b5bc37b809beb0c9e4c42ce6bd1c849b4
Cameron Brewer <morethanfire@icloud.com>
2022-01-05 08:45:41 -0800

    feat: optional props

--- src/dataDisplay/table/table.d.ts
+++ src/dataDisplay/table/table.d.ts
@@ -16,67 +16,68 @@ export interface TablePropTypes {
     /**
      * Override or extend the styles applied to the Table.
      */
-    classes: {
+    classes?: {
+        root: string;
         tableSticky: string;
         tableStickyColumns: string;
     };
     /**
      * Add additional classes to the Table.
      */
-    className: string;
+    className?: string;
     /**
      * If `true`, applies `width: auto` to the Table.
      */
-    collapsing: boolean;
+    collapsing?: boolean;
     /**
      * If `true`, the far left column is divided from the rest of the columns.
      */
-    definition: boolean;
+    definition?: boolean;
     /**
      * If `true`, columns will be evenly spaced.
      */
-    fixed: boolean;
+    fixed?: boolean;
     /**
      * Change the font size of text in the Table.
      */
-    fontSize: 'xxlarge' | 'xlarge' | 'large' | 'medium' | 'small' | 'xsmall' | 'xxsmall';
+    fontSize?: 'xxlarge' | 'xlarge' | 'large' | 'medium' | 'small' | 'xsmall' | 'xxsmall';
     /**
      * Deprecated prop.
      */
-    fullWidth: boolean;
+    fullWidth?: boolean;
     /**
      * Add an id to the Table.
      */
-    id: string;
+    id?: string;
     /**
      * Table rows and cells can be highlighted as if they have been selected.
      */
-    selectable: boolean;
+    selectable?: boolean;
     /**
      * Truncate copy within cells in the Table
      */
-    singleLine: boolean;
+    singleLine?: boolean;
     /**
      * Change the cells vertical size in the Table.
      */
-    size: 'l' | 'large' | 'm' | 'medium' | 's' | 'small';
+    size?: 'l' | 'large' | 'm' | 'medium' | 's' | 'small';
     /**
      * Deprecated prop.
      */
-    stackable: boolean;
+    stackable?: boolean;
     /**
      * A table can have one or more columns defined as 'sticky' so that they stay fixed while the
      * user horizontally scrolls to see the remaining columns.
      */
-    stickyColumnCount: number;
+    stickyColumnCount?: number;
     /**
      * Resizable cell default percentage width related to the Table container size.
      */
-    resizableColumnWidthPercentage: number;
+    resizableColumnWidthPercentage?: number | null;
     /**
      * If `true` or `very`, the Table will bleed off the edge.
      */
-    stretch: boolean | 'very';
+    stretch?: boolean | 'very';
     /**
      * Supply any inline styles to the Table.
      */
