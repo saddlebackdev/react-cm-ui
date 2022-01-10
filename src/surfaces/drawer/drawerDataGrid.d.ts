93676610f53eb9b8e72dcebf2b44ee1a368c8e92
Cameron Brewer <morethanfire@icloud.com>
2022-01-10 09:27:01 -0800

    feat: fixed props

--- src/surfaces/drawer/drawerDataGrid.d.ts
+++ src/surfaces/drawer/drawerDataGrid.d.ts
@@ -1,11 +1,39 @@
+import { number, string } from 'prop-types';
 import React from 'react';
 
 export interface DrawerDataGridPropTypes {
+    bleed?: boolean;
+    classes?: {
+        bleed?: string;
+        root?: string;
+    };
+    className?: string;
+    columns: {}[];
+    data: {}[] | {};
     /**
      * Used for DOM testing. https://testing-library.com/docs/queries/bytestid/
      */
-     dataTestId?: string;
-     id?: string;
+    dataTestId?: string;
+    fontSize?: string;
+    id?: string;
+    minWidth?: number;
+    moduleType?: 'drawer' | 'page';
+    resizableColumnWidthPercentage?: number;
+    rowProps?: Function;
+    size?: 'small' | 'medium';
+    sortable?: {
+        disabled?: boolean;
+        filter?: string;
+        group?: string;
+        handle?: boolean;
+        onChange?: Function;
+        sort?: boolean;
+    }[];
+    small?: boolean;
+    stickyColumnWidth?: number;
+    stickyColumns?: number;
+    stretch?: 'very' | boolean;
+    style?: {};
 }
 
 declare const DrawerDataGrid: React.ComponentType<DrawerDataGridPropTypes>;
