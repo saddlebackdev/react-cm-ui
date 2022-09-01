8ada679000d642f53c2b51a4399c55094af158f1
War777 <oscarResendizUaq@gmail.com>
2022-09-01 01:57:14 -0500

    button group - inital prototype

--- src/inputs/buttonGroup/buttonGroup.jsx
+++ src/inputs/buttonGroup/buttonGroup.jsx
@@ -1,15 +1,69 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
-import MuiButtonGroup from '@material-ui/core/ButtonGroup';
+/* eslint-disable linebreak-style */
+import React, {
+    useState,
+    useEffect,
+} from 'react';
+import PropTypes from 'prop-types';
+import MUIButton from '@material-ui/core/Button';
+import MUIButtonGroup from '@material-ui/core/ButtonGroup';
+import {
+    noop,
+} from 'lodash';
 
 function ButtonGroup(props) {
+    const {
+        classes,
+        buttons,
+        defaultSelectedButton,
+        onChange: onChangeProp = noop,
+    } = props;
+
+    const [selectedButton, setSelectedButton] = useState(defaultSelectedButton || buttons[0].value);
+
+    useEffect(() => {
+        onChangeProp(selectedButton);
+    }, [selectedButton, onChangeProp]);
 
     return (
-        <MuiButtonGroup
-            // eslint-disable-next-line linebreak-style, react/jsx-props-no-spreading
-            {...props}
-        />
+        <MUIButtonGroup
+            color="primary"
+            size="large"
+            classes={classes}
+        >
+            {buttons.map((button) => (
+                <MUIButton
+                    key={button.value}
+                    onClick={() => { setSelectedButton(button.value); }}
+                    variant={button.value === selectedButton ? 'contained' : 'outlined'}
+                >
+                    {button.icon}
+                    {button.label}
+                </MUIButton>
+            ))}
+        </MUIButtonGroup>
     );
 }
+
+ButtonGroup.propTypes = {
+    buttons: PropTypes.arrayOf(
+        PropTypes.shape({
+            value: PropTypes.oneOfType([
+                PropTypes.number,
+                PropTypes.string,
+            ]).required,
+            label: PropTypes.string.required,
+            icon: PropTypes.node,
+        }),
+    ).isRequired,
+    classes: PropTypes.shape({}),
+    defaultSelectedButton: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    onChange: PropTypes.func,
+};
+
+ButtonGroup.defaultProps = {
+    classes: null,
+    defaultSelectedButton: null,
+    onChange: noop,
+};
 
 export default ButtonGroup;
