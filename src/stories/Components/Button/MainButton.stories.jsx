00504eb9494e07168d74cb65741d616bc6d3b504
War777 <oscarresendizuaq@gmail.com>
2023-03-16 13:17:48 -0600

    storybook a - main button

--- src/stories/Components/Button/MainButton.stories.jsx
+++ src/stories/Components/Button/MainButton.stories.jsx
@@ -49,7 +49,7 @@ Main.args = {
         </React.Fragment>
     ),
     color: 'default',
-    designVersion: 1,
+    designVersion: 2,
     variant: 'contained',
     // Pill and href bug in the padding bottom
     href: 'https://www.youtube.com/',
