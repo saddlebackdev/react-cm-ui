0ba2b01230dcdfbace11fa68c531787afbee21c6
Geoffrey Roberts <groberts314@yahoo.com>
2024-04-09 13:54:27 -0700

    fix: get my indentation back & tell ESLINT to take a hike! :-P

--- src/dataDisplay/chip/chip.d.ts
+++ src/dataDisplay/chip/chip.d.ts
@@ -3,13 +3,15 @@ import React from 'react';
 export interface ChipPropTypes {
     children?: React.ReactNode;
     className?: string;
+    /* eslint-disable @typescript-eslint/indent */
     color?: 'active' | 'action' | 'alert' |
-    'alternate' | 'bright' | 'condition' |
-    'configuration' | 'disable' | 'highlight' |
-    'inverse' | 'inverse-alternate' | 'light' |
-    'nest' | 'outline' | 'primary' |
-    'secondary' | 'static' | 'subject' |
-    'success' | 'transparent' | 'warning',
+        'alternate' | 'bright' | 'condition' |
+        'configuration' | 'disable' | 'highlight' |
+        'inverse' | 'inverse-alternate' | 'light' |
+        'nest' | 'outline' | 'primary' |
+        'secondary' | 'static' | 'subject' |
+        'success' | 'transparent' | 'warning',
+    /* eslint-enable @typescript-eslint/indent */
     fluid?: boolean;
     inverse?: boolean;
     label?: string;
